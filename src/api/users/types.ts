3b2d740558554961b280a51501031eaca073c431
Zeeshaan Maudarbocus <zeeshaanmaudarbocus@Admins-MacBook-Air.local>
2023-06-21 20:21:31 +0200

    adjust types

--- src/api/users/types.ts
+++ src/api/users/types.ts
@@ -1,6 +1,8 @@
 export interface UserItem {
     display_name: string;
     user_id: string;
+    profile_image: string;
+    reputation: number;
 }
 export interface UsersResponseAPI {
     items: UserItem[]
