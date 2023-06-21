0ec18e4ec0af383baaddd4dbcb9749e1e1d589dc
Zeeshaan Maudarbocus <zeeshaanmaudarbocus@Admins-MacBook-Air.local>
2023-06-21 20:21:18 +0200

    Build UsersList of users

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,17 +1,47 @@
-import React, { FC } from 'react';
-import { Box } from '@mui/material';
-
-import { User } from '../User/User';
+import React, { FC, useState } from 'react';
+import { AccordionDetails, AccordionSummary, Box, Button, Avatar } from '@mui/material';
+import { ExpandMore as ExpandMoreIcon } from '@mui/icons-material';
 
 import { UsersListProps } from './types';
+import { AccordionStyled, Header, Title, Subtitle} from './styles';
 
 
 export const UsersList: FC<UsersListProps> = ({ usersList }) => {
 
+	const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange = (isExpanded: boolean, panel: string) => {
+		setExpanded(isExpanded ? panel : false);
+	};
+
 	return (
 		<Box>
-			{usersList.map((user) => {
-				return <User key={user.user_id} user={user} />
+			{usersList.map(({ user_id, display_name, profile_image, reputation }) => {
+				return (
+					<AccordionStyled
+						key={user_id}
+						expanded={expanded === user_id}
+						onChange={(event, isExpanded) => handleChange(isExpanded, user_id)}
+					>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls={`${user_id}-content`}
+							id={`${user_id}-header`}
+						>
+							<Box>
+								<Avatar alt={display_name} src={profile_image} />
+							</Box>
+							<Header>
+								<Title>{display_name}</Title>
+								<Subtitle color="text.secondary">Reputation: {reputation}</Subtitle>
+							</Header>
+						</AccordionSummary>
+						<AccordionDetails>
+							<Button>Follow</Button>
+							<Button color="error">Block</Button>
+						</AccordionDetails>
+				</AccordionStyled>
+				)
 			})}
 		</Box>
 	);
---
+++ src/components/UsersList/styles.ts
@@ -0,0 +1,17 @@
+import { styled, Accordion, Typography, Stack } from '@mui/material';
+
+export const AccordionStyled = styled(Accordion)`
+	margin-bottom: 10px;
+`;
+
+export const Header = styled(Stack)`
+	padding-left: 10px;
+`;
+
+export const Title = styled(Typography)`
+	font-size: 18px;
+`;
+
+export const Subtitle = styled(Typography)`
+	font-size: 12px;
+`;
